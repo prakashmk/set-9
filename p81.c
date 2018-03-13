132654a44320945b8a1bd2f9d7c0e0512236cfce
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-03-13 16:33:46 +0530

    Update p81.c

--- p81.c
+++ p81.c
@@ -2,10 +2,10 @@
 int main() 
  {
 	int n,o,d;
-	printf("\nEnter the number : ");
+	printf("\nEnter the counts of both clan : ");
 	scanf("%2d%2d",&n,&o);
 	 d=o-n;
-	 printf("\nThe Difference between given numbers is : %d",d);
+	 printf("\nThe Difference between the two clans is : %d",d);
   
 	return 0;
  }
