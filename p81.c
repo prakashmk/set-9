40cee39f6b5a9f04fea92511b942becadf4e5e1f
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-03-13 16:27:21 +0530

    Create p81.c

---
+++ p81.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+ {
+	int n,o,d;
+	printf("\nEnter the number : ");
+	scanf("%2d%2d",&n,&o);
+	 d=o-n;
+	 printf("\nThe Difference between given numbers is : %d",d);
+  
+	return 0;
+ }
