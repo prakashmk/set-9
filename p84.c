e668990258dd4220ae211e726ff24092fa622be4
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-03-13 16:49:22 +0530

    Create p84.c

---
+++ p84.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+ char ch;
+ printf("\n Please Enter any character \n");
+ scanf("%c",&ch);
+ printf("\n The ASCII value of given character = %d",ch);
+ return 0;
+}
